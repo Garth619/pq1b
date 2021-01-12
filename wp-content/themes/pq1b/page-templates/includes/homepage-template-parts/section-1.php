61bb0bf6c4e54efb84f82c76ab587970d701ddb9
Garrett Cullen <Garth619@users.noreply.github.com>
2021-01-11 21:13:18 -0800

    push

--- wp-content/themes/pq1b/page-templates/includes/homepage-template-parts/section-1.php
+++ wp-content/themes/pq1b/page-templates/includes/homepage-template-parts/section-1.php
@@ -1,4 +1,14 @@
 <section id="section-one">
-
+  <div class="sec-one-inner">
+    <span class="sec-one-title">“Aggressive, unorthodox and top-notch, a record of high-stakes wins, and top
+      talent.”</span> <!-- /.sec-one-title -->
+    <span class="sec-one-name">-Daily Journal</span> <!-- /.sec-one-name -->
+  </div>
+  <!-- /.sec-one-inner -->
+  <picture>
+    <source media='(min-width: 768px)' srcset='<?php bloginfo('template_directory');?>/images/main-hero-tablet.jpg'>
+    <img id='hero' src='<?php bloginfo('template_directory');?>/images/main-hero-mobile.jpg' alt='' />
+  </picture>
+  <!-- remove 'data-' and 'lazyload' if needs to load on page load -->
 </section>
 <!-- /#section-one -->
