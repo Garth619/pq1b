a23fcc43117562d3f29eab857a19efdc18b99dda
Garrett Cullen <Garth619@users.noreply.github.com>
2021-01-13 12:58:40 -0800

    push

--- wp-content/themes/pq1b/page-templates/includes/homepage-template-parts/section-7.php
+++ wp-content/themes/pq1b/page-templates/includes/homepage-template-parts/section-7.php
@@ -2,65 +2,32 @@
 
   <div id='sec-seven-inner'>
 
+    <span id='sec-seven-title'>We’ll see you in court. Our court</span><!-- sec-seven-title -->
+
     <div id='sec-seven-top'>
 
-      <span id='sec-seven-title'><?php the_field('section_seven_title');?></span><!-- sec-seven-title -->
+      <div id='sec-seven-img-wrapper'>
 
-      <div id='sec-seven-content' class="content">
+        <img src='<?php bloginfo('template_directory');?>/images/.jpg' alt='' />
 
-        <?php the_field('section_seven_intro');?>
-
-      </div><!-- sec-seven-content -->
+      </div><!-- sec-seven-img-wrapper -->
 
     </div><!-- sec-seven-top -->
 
-    <div id='sec-seven-bottom'>
+    <div id='sec-seven-bottom' class="content">
 
-      <div class='sec-seven-faq-col'>
+      <div id='sec-seven-intro'>
 
-        <?php if (have_rows('section_seven_faqs_column_one')): ?>
-        <?php while (have_rows('section_seven_faqs_column_one')): the_row();?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua.</p>
 
-        <div class='sec-seven-faq'>
+      </div><!-- sec-seven-intro -->
 
-          <span class='sec-seven-question'><?php the_sub_field('question');?></span><!-- sec-seven-question -->
-
-          <div class='sec-seven-answer content'>
-
-            <?php the_sub_field('answer');?>
-
-          </div><!-- sec-seven-answer -->
-
-        </div><!-- sec-seven-faq -->
-
-        <?php endwhile;?>
-        <?php endif;?>
-
-      </div><!-- sec-seven-faq-col -->
-
-      <div class='sec-seven-faq-col'>
-
-        <?php if (have_rows('section_seven_faqs_column_two')): ?>
-        <?php while (have_rows('section_seven_faqs_column_two')): the_row();?>
-
-        <div class='sec-seven-faq'>
-
-          <span class='sec-seven-question'><?php the_sub_field('question');?></span><!-- sec-seven-question -->
-
-          <div class='sec-seven-answer content'>
-
-            <?php the_sub_field('answer');?>
-
-          </div><!-- sec-seven-answer -->
-
-        </div><!-- sec-seven-faq -->
-
-        <?php endwhile;?>
-        <?php endif;?>
-
-      </div><!-- sec-seven-faq-col -->
-
-      <a class='button sec-seven-button' href=''>View all FAQs</a><!-- button sec-seven-button -->
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa.</p>
 
     </div><!-- sec-seven-bottom -->
 
