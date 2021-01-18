a6e77afc5d8a143e3202ed182101f03b6ee3f286
Garrett Cullen <Garth619@users.noreply.github.com>
2021-01-18 12:35:54 -0800

    push

--- wp-content/themes/pq1b/footer.php
+++ wp-content/themes/pq1b/footer.php
@@ -31,7 +31,7 @@
 
         <span class='location-title'>Los Angeles</span><!-- location-title -->
 
-        <span class='location-address'>601 S. Figueroa Street 30th Floor Los Angeles, CA 90017</span>
+        <span class='location-address'>601 S. Figueroa Street<br /> 30th Floor<br /> Los Angeles, CA 90017</span>
         <!-- location-address -->
 
         <span class='location-phone'>Phone <span>(213) 334-7000</span></span><!-- location-phone -->
@@ -44,7 +44,7 @@
 
         <span class='location-title'>Orange County</span><!-- location-title -->
 
-        <span class='location-address'>605 Town Center Drive Suite 1700 Costa Mesa, CA 92626</span>
+        <span class='location-address'>605 Town Center Drive<br /> Suite 1700<br /> Costa Mesa, CA 92626</span>
         <!-- location-address -->
 
         <span class='location-phone'>Phone <span>(949) 383-2800</span></span><!-- location-phone -->
