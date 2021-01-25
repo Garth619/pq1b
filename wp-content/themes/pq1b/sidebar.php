5af47fe4abb5c11c227533e463d282b36981883d
Garrett Cullen <Garth619@users.noreply.github.com>
2021-01-24 20:49:33 -0800

    push

--- wp-content/themes/pq1b/sidebar.php
+++ wp-content/themes/pq1b/sidebar.php
@@ -31,9 +31,155 @@
 
             <div class='single-featured-att-content'>
 
-              <span class='single-featured-att-title'>Alan A. Greenberg</span><!-- single-featured-att-title -->
+              <div class='single-featured-att-content-inner'>
 
-              <span class='single-featured-att-position'>Co-Founding Partner</span><!-- single-featured-att-position -->
+                <span class='single-featured-att-title'>Alan A. Greenberg</span><!-- single-featured-att-title -->
+
+                <span class='single-featured-att-position'>Co-Founding Partner</span>
+
+                <!-- single-featured-att-position -->
+
+              </div><!-- single-featured-att-content-inner -->
+
+            </div><!-- single-featured-att-content -->
+
+          </div><!-- single-featured-att-inner -->
+
+        </a><!-- single-featured-att -->
+
+        <a href="" class='single-featured-att'>
+
+          <div class='single-featured-att-inner'>
+
+            <div class='single-featured-att-img'>
+
+              <img src='<?php bloginfo('template_directory');?>/images/att-borges.jpg' alt='' />
+
+            </div><!-- single-featured-att-img -->
+
+            <div class='single-featured-att-content'>
+
+              <div class='single-featured-att-content-inner'>
+
+                <span class='single-featured-att-title'>Alan A. Greenberg</span><!-- single-featured-att-title -->
+
+                <span class='single-featured-att-position'>Co-Founding Partner</span>
+
+                <!-- single-featured-att-position -->
+
+              </div><!-- single-featured-att-content-inner -->
+
+            </div><!-- single-featured-att-content -->
+
+          </div><!-- single-featured-att-inner -->
+
+        </a><!-- single-featured-att -->
+
+        <a href="" class='single-featured-att'>
+
+          <div class='single-featured-att-inner'>
+
+            <div class='single-featured-att-img'>
+
+              <img src='<?php bloginfo('template_directory');?>/images/att-borges.jpg' alt='' />
+
+            </div><!-- single-featured-att-img -->
+
+            <div class='single-featured-att-content'>
+
+              <div class='single-featured-att-content-inner'>
+
+                <span class='single-featured-att-title'>Alan A. Greenberg</span><!-- single-featured-att-title -->
+
+                <span class='single-featured-att-position'>Co-Founding Partner</span>
+
+                <!-- single-featured-att-position -->
+
+              </div><!-- single-featured-att-content-inner -->
+
+            </div><!-- single-featured-att-content -->
+
+          </div><!-- single-featured-att-inner -->
+
+        </a><!-- single-featured-att -->
+
+        <a href="" class='single-featured-att'>
+
+          <div class='single-featured-att-inner'>
+
+            <div class='single-featured-att-img'>
+
+              <img src='<?php bloginfo('template_directory');?>/images/att-borges.jpg' alt='' />
+
+            </div><!-- single-featured-att-img -->
+
+            <div class='single-featured-att-content'>
+
+              <div class='single-featured-att-content-inner'>
+
+                <span class='single-featured-att-title'>Alan A. Greenberg</span><!-- single-featured-att-title -->
+
+                <span class='single-featured-att-position'>Co-Founding Partner</span>
+
+                <!-- single-featured-att-position -->
+
+              </div><!-- single-featured-att-content-inner -->
+
+            </div><!-- single-featured-att-content -->
+
+          </div><!-- single-featured-att-inner -->
+
+        </a><!-- single-featured-att -->
+
+        <a href="" class='single-featured-att'>
+
+          <div class='single-featured-att-inner'>
+
+            <div class='single-featured-att-img'>
+
+              <img src='<?php bloginfo('template_directory');?>/images/att-borges.jpg' alt='' />
+
+            </div><!-- single-featured-att-img -->
+
+            <div class='single-featured-att-content'>
+
+              <div class='single-featured-att-content-inner'>
+
+                <span class='single-featured-att-title'>Alan A. Greenberg</span><!-- single-featured-att-title -->
+
+                <span class='single-featured-att-position'>Co-Founding Partner</span>
+
+                <!-- single-featured-att-position -->
+
+              </div><!-- single-featured-att-content-inner -->
+
+            </div><!-- single-featured-att-content -->
+
+          </div><!-- single-featured-att-inner -->
+
+        </a><!-- single-featured-att -->
+
+        <a href="" class='single-featured-att'>
+
+          <div class='single-featured-att-inner'>
+
+            <div class='single-featured-att-img'>
+
+              <img src='<?php bloginfo('template_directory');?>/images/att-borges.jpg' alt='' />
+
+            </div><!-- single-featured-att-img -->
+
+            <div class='single-featured-att-content'>
+
+              <div class='single-featured-att-content-inner'>
+
+                <span class='single-featured-att-title'>Alan A. Greenberg</span><!-- single-featured-att-title -->
+
+                <span class='single-featured-att-position'>Co-Founding Partner</span>
+
+                <!-- single-featured-att-position -->
+
+              </div><!-- single-featured-att-content-inner -->
 
             </div><!-- single-featured-att-content -->
 
@@ -45,9 +191,9 @@
 
       <div id='featured-att-button-wrapper'>
 
-        <a href="" class='butto-three featured-att-button'>MORE attorneys</a><!-- butto-three featured-att-button -->
+        <a href="" class='button-three featured-att-button'>MORE attorneys</a><!-- button-three featured-att-button -->
 
-        <a href="" class='butto-three featured-att-button'>VIEW ALL</a><!-- butto-three featured-att-button -->
+        <a href="" class='button-three featured-att-button'>VIEW ALL</a><!-- button-three featured-att-button -->
 
       </div><!-- featured-att-button-wrapper -->
 
