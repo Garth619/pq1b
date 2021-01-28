764e92c0ffd0729b0fcd3105351bb3c197a634e7
Garrett Cullen <Garth619@users.noreply.github.com>
2021-01-28 12:21:07 -0800

    push

--- wp-content/themes/pq1b/page-templates/template-testimonials.php
+++ wp-content/themes/pq1b/page-templates/template-testimonials.php
@@ -25,16 +25,10 @@ get_header();?>
           <p>“I always felt he genuinely cared
             and is a credit to his profession.</p>
 
-          <p>“I always felt he genuinely cared
-            and is a credit to his profession.</p>
 
         </div><!-- testi-quote -->
 
         <div class='testi-descrip content'>
-
-          <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to be
-            expected from the court the DA’s office, explained all my options. I felt that he was very competent and
-            with his confidence he set my mind at ease.”</p>
 
           <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to be
             expected from the court the DA’s office, explained all my options. I felt that he was very competent and
@@ -52,61 +46,136 @@ get_header();?>
 
       <div class='testi-col'>
 
-        <img class='stars' src='<?php bloginfo('template_directory');?>/images/stars.svg' alt='' />
+        <div class='testi-bottom-single'>
 
-        <div class='testi-quote'>
+          <img class='stars' src='<?php bloginfo('template_directory');?>/images/stars.svg' alt='' />
 
-          <p>“I always felt he genuinely cared
-            and is a credit to his profession.</p>
+          <div class='testi-quote'>
 
-          <p>“I always felt he genuinely cared
-            and is a credit to his profession.</p>
+            <p>“I always felt he genuinely cared
+              and is a credit to his profession.</p>
 
-        </div><!-- testi-quote -->
+            <p>“I always felt he genuinely cared
+              and is a credit to his profession.</p>
 
-        <div class='testi-descrip content'>
+          </div><!-- testi-quote -->
 
-          <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to be
-            expected from the court the DA’s office, explained all my options. I felt that he was very competent and
-            with his confidence he set my mind at ease.”</p>
+          <div class='testi-descrip content'>
 
-          <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to be
-            expected from the court the DA’s office, explained all my options. I felt that he was very competent and
-            with his confidence he set my mind at ease.”</p>
+            <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to
+              be
+              expected from the court the DA’s office, explained all my options. I felt that he was very competent and
+              with his confidence he set my mind at ease.”</p>
 
-        </div><!-- testi-descrip content -->
+            <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to
+              be
+              expected from the court the DA’s office, explained all my options. I felt that he was very competent and
+              with his confidence he set my mind at ease.”</p>
 
-        <span class='testi-name'>David Murphey</span><!-- testi-name -->
+          </div><!-- testi-descrip content -->
+
+          <span class='testi-name'>David Murphey</span><!-- testi-name -->
+
+        </div><!-- testi-bottom-single -->
+
+        <div class='testi-bottom-single'>
+
+          <img class='stars' src='<?php bloginfo('template_directory');?>/images/stars.svg' alt='' />
+
+          <div class='testi-quote'>
+
+            <p>“I always felt he genuinely cared
+              and is a credit to his profession.</p>
+
+            <p>“I always felt he genuinely cared
+              and is a credit to his profession.</p>
+
+          </div><!-- testi-quote -->
+
+          <div class='testi-descrip content'>
+
+            <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to
+              be
+              expected from the court the DA’s office, explained all my options. I felt that he was very competent and
+              with his confidence he set my mind at ease.”</p>
+
+            <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to
+              be
+              expected from the court the DA’s office, explained all my options. I felt that he was very competent and
+              with his confidence he set my mind at ease.”</p>
+
+          </div><!-- testi-descrip content -->
+
+          <span class='testi-name'>David Murphey</span><!-- testi-name -->
+
+        </div><!-- testi-bottom-single -->
 
       </div><!-- testi-col -->
 
       <div class='testi-col'>
 
-        <img class='stars' src='<?php bloginfo('template_directory');?>/images/stars.svg' alt='' />
+        <div class='testi-bottom-single'>
 
-        <div class='testi-quote'>
+          <img class='stars' src='<?php bloginfo('template_directory');?>/images/stars.svg' alt='' />
 
-          <p>“I always felt he genuinely cared
-            and is a credit to his profession.</p>
+          <div class='testi-quote'>
 
-          <p>“I always felt he genuinely cared
-            and is a credit to his profession.</p>
+            <p>“I always felt he genuinely cared
+              and is a credit to his profession.</p>
 
-        </div><!-- testi-quote -->
+            <p>“I always felt he genuinely cared
+              and is a credit to his profession.</p>
 
-        <div class='testi-descrip content'>
+          </div><!-- testi-quote -->
 
-          <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to be
-            expected from the court the DA’s office, explained all my options. I felt that he was very competent and
-            with his confidence he set my mind at ease.”</p>
+          <div class='testi-descrip content'>
 
-          <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to be
-            expected from the court the DA’s office, explained all my options. I felt that he was very competent and
-            with his confidence he set my mind at ease.”</p>
+            <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to
+              be
+              expected from the court the DA’s office, explained all my options. I felt that he was very competent and
+              with his confidence he set my mind at ease.”</p>
 
-        </div><!-- testi-descrip content -->
+            <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to
+              be
+              expected from the court the DA’s office, explained all my options. I felt that he was very competent and
+              with his confidence he set my mind at ease.”</p>
 
-        <span class='testi-name'>David Murphey</span><!-- testi-name -->
+          </div><!-- testi-descrip content -->
+
+          <span class='testi-name'>David Murphey</span><!-- testi-name -->
+
+        </div><!-- testi-bottom-single -->
+        <div class='testi-bottom-single'>
+
+          <img class='stars' src='<?php bloginfo('template_directory');?>/images/stars.svg' alt='' />
+
+          <div class='testi-quote'>
+
+            <p>“I always felt he genuinely cared
+              and is a credit to his profession.</p>
+
+            <p>“I always felt he genuinely cared
+              and is a credit to his profession.</p>
+
+          </div><!-- testi-quote -->
+
+          <div class='testi-descrip content'>
+
+            <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to
+              be
+              expected from the court the DA’s office, explained all my options. I felt that he was very competent and
+              with his confidence he set my mind at ease.”</p>
+
+            <p>David right away set me at ease with the case he was handling for me, he explained in detail what was to
+              be
+              expected from the court the DA’s office, explained all my options. I felt that he was very competent and
+              with his confidence he set my mind at ease.”</p>
+
+          </div><!-- testi-descrip content -->
+
+          <span class='testi-name'>David Murphey</span><!-- testi-name -->
+
+        </div><!-- testi-bottom-single -->
 
       </div><!-- testi-col -->
 
