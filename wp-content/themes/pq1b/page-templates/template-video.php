712e18895e473493055d391f48673929eb171c35
Garrett Cullen <Garth619@users.noreply.github.com>
2021-01-29 14:54:52 -0800

    push

--- wp-content/themes/pq1b/page-templates/template-video.php
+++ wp-content/themes/pq1b/page-templates/template-video.php
@@ -25,19 +25,83 @@ get_header();?>
 
         <a href="https://www.youtube.com/embed/<?php the_sub_field('youtube_id_video_center');?>" data-lity>
 
+          <div class='single-video-inner'>
+
+            <div class='pq-video-img'>
+
+              <div class='pq-video-overlay'>
+
+                <span class='pq-play-button'></span><!-- pq-play-button -->
+
+              </div><!-- pq-video-overlay -->
+
+              <?php if (get_sub_field('video_thumbnail_select') == "Leave Blank") {?>
+
+              <img src="https://img.youtube.com/vi/<?php the_sub_field('youtube_id_video_center');?>/0.jpg" />
+
+              <?php }?>
+
+              <?php if (get_sub_field('video_thumbnail_select') == "Add Thumbnail Image") {?>
+
+              <?php $video_thumbnail_video_center = get_sub_field('video_thumbnail_video_center');?>
+
+              <img src="<?php echo $video_thumbnail_video_center['url']; ?>"
+                alt="<?php echo $video_thumbnail_video_center['alt']; ?>" />
+
+              <?php }?>
+
+            </div><!-- pq-video-img -->
+
+            <div class='video-title-wrapper'>
+
+              <span class='video-title'><?php the_sub_field('video_title_video_center');?></span><!-- video_title -->
+
+            </div><!-- video_title_wrapper -->
+
+          </div><!-- single-video-inner -->
+
+        </a>
+
+        <div class='pq-video-line'></div><!-- pq-video-line -->
+
+      </div><!-- single_video -->
+
+      <?php }?>
+
+      <?php if (get_field('wistia_or_youtube') == "Wistia") {?>
+
+      <div class='single-video pq-video'>
+
+        <div class='single-video-inner'>
+
           <div class='pq-video-img'>
+
+            <?php if (get_sub_field('video_thumbnail_select') == "Add Thumbnail Image") {?>
+
+            <div
+              class='mywistia wistia_embed wistia_async_<?php the_sub_field('wistia_id_video_center');?> popover=true popoverContent=html'>
+            </div><!-- mywistia -->
+
+            <?php }?>
+
+            <?php if (get_sub_field('video_thumbnail_select') == "Leave Blank") {?>
+
+            <div class="mywistia-thumbnail">
+
+              <div
+                class='mywistia wistia_embed wistia_async_<?php the_sub_field('wistia_id_video_center');?> popover=true popoverContent=thumbnail'>
+              </div><!-- mywistia -->
+
+            </div>
+            <!-- mywistia-thumbnail -->
+
+            <?php }?>
 
             <div class='pq-video-overlay'>
 
               <span class='pq-play-button'></span><!-- pq-play-button -->
 
             </div><!-- pq-video-overlay -->
-
-            <?php if (get_sub_field('video_thumbnail_select') == "Leave Blank") {?>
-
-            <img src="https://img.youtube.com/vi/<?php the_sub_field('youtube_id_video_center');?>/0.jpg" />
-
-            <?php }?>
 
             <?php if (get_sub_field('video_thumbnail_select') == "Add Thumbnail Image") {?>
 
@@ -52,66 +116,13 @@ get_header();?>
 
           <div class='video-title-wrapper'>
 
-            <span class='video-title'><?php the_sub_field('video_title_video_center');?></span><!-- video_title -->
+            <span class='video-title'><?php the_sub_field('video_title_video_center');?></span><!-- video-title -->
 
-          </div><!-- video_title_wrapper -->
+          </div><!-- video-title-wrapper -->
 
-        </a>
+        </div><!-- single-video-inner -->
 
         <div class='pq-video-line'></div><!-- pq-video-line -->
-
-      </div><!-- single_video -->
-
-      <?php }?>
-
-      <?php if (get_field('wistia_or_youtube') == "Wistia") {?>
-
-      <div class='single-video'>
-
-        <div class='single-video-thumbnail'>
-
-          <?php if (get_sub_field('video_thumbnail_select') == "Add Thumbnail Image") {?>
-
-          <div
-            class='mywistia wistia_embed wistia_async_<?php the_sub_field('wistia_id_video_center');?> popover=true popoverContent=html'>
-          </div><!-- mywistia -->
-
-          <?php }?>
-
-          <?php if (get_sub_field('video_thumbnail_select') == "Leave Blank") {?>
-
-          <div class="mywistia-thumbnail">
-
-            <div
-              class='mywistia wistia_embed wistia_async_<?php the_sub_field('wistia_id_video_center');?> popover=true popoverContent=thumbnail'>
-            </div><!-- mywistia -->
-
-          </div><!-- mywistia-thumbnail -->
-
-          <?php }?>
-
-          <div class='video-overlay'>
-
-
-
-          </div><!-- video-overlay -->
-
-          <?php if (get_sub_field('video_thumbnail_select') == "Add Thumbnail Image") {?>
-
-          <?php $video_thumbnail_video_center = get_sub_field('video_thumbnail_video_center');?>
-
-          <img src="<?php echo $video_thumbnail_video_center['url']; ?>"
-            alt="<?php echo $video_thumbnail_video_center['alt']; ?>" />
-
-          <?php }?>
-
-        </div><!-- single-video-thumbnail -->
-
-        <div class='video-title-wrapper'>
-
-          <span class='video-title'><?php the_sub_field('video_title_video_center');?></span><!-- video-title -->
-
-        </div><!-- video-title-wrapper -->
 
       </div><!-- single-video -->
 
@@ -128,3 +139,9 @@ get_header();?>
 </div><!-- internal-main -->
 
 <?php get_footer();?>
+
+<?php if (get_field('wistia_or_youtube') == "Wistia") {?>
+
+<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+
+<?php }?>
