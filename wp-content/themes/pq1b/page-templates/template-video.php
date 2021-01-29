a98af2da59327fc8aff8295c725ac631a1fd4834
Garrett Cullen <Garth619@users.noreply.github.com>
2021-01-28 18:02:15 -0800

    push

--- wp-content/themes/pq1b/page-templates/template-video.php
+++ wp-content/themes/pq1b/page-templates/template-video.php
@@ -6,32 +6,120 @@ get_header();?>
 
 <div id="internal-main">
 
-  <h1 class="page-title page-large-content-title"><?php the_title();?></h1>
-
   <div id="page-container">
-    <!-- do yt wsta with thumbnail options -->
+
+    <div id='large-title-wrapper'>
+
+      <h1 class="page-title large-title no-banner-title"><?php the_title();?></h1>
+
+    </div><!-- large-title-wrapper -->
+
     <div id='video-wrapper'>
 
-      <div id='single-video' class="pq-video">
+      <?php if (have_rows('video_center_')): ?>
+      <?php while (have_rows('video_center_')): the_row();?>
 
-        <div class='mywistia'></div><!-- mywistia -->
+      <?php if (get_field('wistia_or_youtube') == "Youtube") {?>
 
-        <div class='pq-video-img'>
+      <div class='single-video pq-video'>
 
-          <img src='<?php bloginfo('template_directory');?>/images/video-image.jpg' alt='' />
+        <a href="https://www.youtube.com/embed/<?php the_sub_field('youtube_id_video_center');?>" data-lity>
 
-          <div class='pq-video-overlay'>
+          <div class='pq-video-img'>
 
-            <span class='pq-play-button'></span><!-- pq-play-button -->
+            <div class='pq-video-overlay'>
 
-          </div><!-- pq-video-overlay -->
+              <span class='pq-play-button'></span><!-- pq-play-button -->
 
-        </div><!-- pq-video-img -->
+            </div><!-- pq-video-overlay -->
 
-        <span class='single-video-title-wrapper'>Learn about our law firm really long title that is two lines long here
-          is the style</span><!-- single-video-title-wrapper -->
+            <?php if (get_sub_field('video_thumbnail_select') == "Leave Blank") {?>
+
+            <img src="https://img.youtube.com/vi/<?php the_sub_field('youtube_id_video_center');?>/0.jpg" />
+
+            <?php }?>
+
+            <?php if (get_sub_field('video_thumbnail_select') == "Add Thumbnail Image") {?>
+
+            <?php $video_thumbnail_video_center = get_sub_field('video_thumbnail_video_center');?>
+
+            <img src="<?php echo $video_thumbnail_video_center['url']; ?>"
+              alt="<?php echo $video_thumbnail_video_center['alt']; ?>" />
+
+            <?php }?>
+
+          </div><!-- pq-video-img -->
+
+          <div class='video-title-wrapper'>
+
+            <span class='video-title'><?php the_sub_field('video_title_video_center');?></span><!-- video_title -->
+
+          </div><!-- video_title_wrapper -->
+
+        </a>
+
+        <div class='pq-video-line'></div><!-- pq-video-line -->
+
+      </div><!-- single_video -->
+
+      <?php }?>
+
+      <?php if (get_field('wistia_or_youtube') == "Wistia") {?>
+
+      <div class='single-video'>
+
+        <div class='single-video-thumbnail'>
+
+          <?php if (get_sub_field('video_thumbnail_select') == "Add Thumbnail Image") {?>
+
+          <div
+            class='mywistia wistia_embed wistia_async_<?php the_sub_field('wistia_id_video_center');?> popover=true popoverContent=html'>
+          </div><!-- mywistia -->
+
+          <?php }?>
+
+          <?php if (get_sub_field('video_thumbnail_select') == "Leave Blank") {?>
+
+          <div class="mywistia-thumbnail">
+
+            <div
+              class='mywistia wistia_embed wistia_async_<?php the_sub_field('wistia_id_video_center');?> popover=true popoverContent=thumbnail'>
+            </div><!-- mywistia -->
+
+          </div><!-- mywistia-thumbnail -->
+
+          <?php }?>
+
+          <div class='video-overlay'>
+
+
+
+          </div><!-- video-overlay -->
+
+          <?php if (get_sub_field('video_thumbnail_select') == "Add Thumbnail Image") {?>
+
+          <?php $video_thumbnail_video_center = get_sub_field('video_thumbnail_video_center');?>
+
+          <img src="<?php echo $video_thumbnail_video_center['url']; ?>"
+            alt="<?php echo $video_thumbnail_video_center['alt']; ?>" />
+
+          <?php }?>
+
+        </div><!-- single-video-thumbnail -->
+
+        <div class='video-title-wrapper'>
+
+          <span class='video-title'><?php the_sub_field('video_title_video_center');?></span><!-- video-title -->
+
+        </div><!-- video-title-wrapper -->
 
       </div><!-- single-video -->
+
+      <?php }?>
+
+      <?php endwhile;?>
+
+      <?php endif;?>
 
     </div><!-- video-wrapper -->
 
