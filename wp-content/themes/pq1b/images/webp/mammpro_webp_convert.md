71f69587495f71bda04742ee1fa30c23caaf83c5
Garrett Cullen <Garth619@users.noreply.github.com>
2021-02-01 12:27:22 -0800

    push

--- wp-content/themes/pq1b/images/webp/mammpro_webp_convert.md
+++
@@ -1,6 +0,0 @@
-run this command if you have Imagick with Webp installed on Mamp Pro
-
-http://www.imagemagick.org/Usage/basics/#mogrify
-
-whole folder:
-/Applications/MAMP/Library/bin/mogrify -format webp /Applications/MAMP/htdocs/NAME/wp-content/themes/NAME/images/webp/\*.jpg
--- wp-content/themes/pq1b/page-templates/includes/homepage-template-parts/section-1.php
+++ wp-content/themes/pq1b/page-templates/includes/homepage-template-parts/section-1.php
@@ -10,9 +10,21 @@
 
   <picture>
 
+    <?php $section_one_monitor_image_webp = get_field('section_one_monitor_image_webp');?>
+    <?php if ($section_one_monitor_image_webp) {?>
+    <source media='(min-width: 1695px)' srcset='<?php echo $section_one_monitor_image_webp['url']; ?>'
+      type='image/webp'>
+    <?php }?>
+
     <?php $section_one_monitor_image = get_field('section_one_monitor_image');?>
     <?php if ($section_one_monitor_image) {?>
     <source media='(min-width: 1695px)' srcset='<?php echo $section_one_monitor_image['url']; ?>'>
+    <?php }?>
+
+    <?php $section_one_large_laptop_image_webp = get_field('section_one_large_laptop_image_webp');?>
+    <?php if ($section_one_large_laptop_image_webp) {?>
+    <source media='(min-width: 1380px)' srcset='<?php echo $section_one_large_laptop_image_webp['url']; ?>'
+      type='image/webp'>
     <?php }?>
 
     <?php $section_one_large_laptop_image = get_field('section_one_large_laptop_image');?>
@@ -20,16 +32,30 @@
     <source media='(min-width: 1380px)' srcset='<?php echo $section_one_large_laptop_image['url']; ?>'>
     <?php }?>
 
+    <?php $section_one_small_laptop_image_webp = get_field('section_one_small_laptop_image_webp');?>
+    <?php if ($section_one_small_laptop_image_webp) {?>
+    <source media='(min-width: 1170px)' srcset='<?php echo $section_one_small_laptop_image_webp['url']; ?>'
+      type='image/webp'>
+    <?php }?>
 
     <?php $section_one_small_laptop_image = get_field('section_one_small_laptop_image');?>
     <?php if ($section_one_small_laptop_image) {?>
     <source media='(min-width: 1170px)' srcset='<?php echo $section_one_small_laptop_image['url']; ?>'>
     <?php }?>
 
+    <?php $section_one_tablet_image_webp = get_field('section_one_tablet_image_webp');?>
+    <?php if ($section_one_tablet_image_webp) {?>
+    <source media='(min-width: 768px)' srcset='<?php echo $section_one_tablet_image_webp['url']; ?>' type='image/webp'>
+    <?php }?>
 
     <?php $section_one_tablet_image = get_field('section_one_tablet_image');?>
     <?php if ($section_one_tablet_image) {?>
     <source media='(min-width: 768px)' srcset='<?php echo $section_one_tablet_image['url']; ?>'>
+    <?php }?>
+
+    <?php $section_one_mobile_image_webp = get_field('section_one_mobile_image_webp');?>
+    <?php if ($section_one_mobile_image_webp) {?>
+    <source srcset='<?php echo $section_one_mobile_image_webp['url']; ?>' type='image/webp'>
     <?php }?>
 
     <?php $section_one_mobile_image = get_field('section_one_mobile_image');?>
@@ -37,7 +63,7 @@
     <img id='hero' src="<?php echo $section_one_mobile_image['url']; ?>"
       alt="<?php echo $section_one_mobile_image['alt']; ?>" />
 
-
+  </picture>
 
 
 
